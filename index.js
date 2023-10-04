36a115bb4874972080abb199b4922da467518f30
Yuhan224 <fir3panic@gmail.com>
2023-10-04 09:23:43 -0400

    wasdwas

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+const height= number (prompt("What is your height"))
+const weight= number (prompt("How tall are you?"))
+
 function test(BMI) {
     console.log (BMI)
 }
+
+test ("How big are you buddy")
