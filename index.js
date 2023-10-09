30dbe738c06edb4791bb3e6f073b06fb336f9188
Yuhan224 <fir3panic@gmail.com>
2023-10-09 12:46:40 -0400

    aaaa

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const username = prompt('What is your name?');
-const height = prompt("What is your height");
-const weight = prompt("How fat are you?");
+const height = prompt("What is your height in cm");
+const weight = prompt("What is your weight in KG?");
 
-function findBMI(height, weight) {
-    return (weight / height ** 2) * 703
-};
-
-BMI = findBMI(height, weight);
+function FindBMI(height, weight) {
+    return (weight / height ** 2) * 10000
+}
+BMI = FindBMI(height, weight);
 console.log(username + 'your bmi is ' + BMI); 
+
+if (BMI <= 18.5) {
+    console.log('You are underweight');
+}
+else if (BMI <= 24.9) {
+    console.log('You are healthy');
+}
+else if (BMI >= 24.9) {
+    console.log('You are unhealthy');
+}
