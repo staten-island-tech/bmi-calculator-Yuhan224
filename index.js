9946a9407cc17d769cda7472453ed56e95e656d7
Yuhan224 <fir3panic@gmail.com>
2023-10-09 21:03:49 -0400

    Update index.js

--- index.js
+++ index.js
@@ -9,11 +9,13 @@ BMI = FindBMI(height, weight);
 console.log(username + 'your bmi is ' + BMI); 
 
 if (BMI <= 18.5) {
-    console.log('You are underweight');
+    console.log('You are underweight go eat something nice');
 }
 else if (BMI <= 24.9) {
-    console.log('You are healthy');
+    console.log('You are healthy keep it up buddy');
 }
 else if (BMI >= 24.9) {
-    console.log('You are unhealthy');
+    console.log('You are unhealthy lets figure this out shall we');
 }
+
+
